1b7c5c5a2b9bfdca6ed6ce46c8b441abfd3e6193
Shakib <shakib.k2000@gmail.com>
2024-02-18 22:45:19 +0600

    added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
-
+import './Header.css'
+import logo from '../../images/Logo.svg'
 const Header = () => {
     return (
         <nav className='header'>
+
             <img src={logo} alt="" />
+
             <div>
-                <a href="/shop">Shop</a>
-                <a href="/order">Orders</a>
+                <a href="/shop">shop</a>
+                <a href="/order">order</a>
                 <a href="/inventory">Inventory</a>
-                <a href="/login">Login</a>
+                <a href="/Login">Login</a>
             </div>
         </nav>
     );
